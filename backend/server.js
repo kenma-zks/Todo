fe52c38511ef29909f51e59f8cb0e0678fa0c412
kenma-zks <pallawmgr20@gmail.com>
2023-09-24 16:09:53 +0545

    fix

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,22 @@ import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 connectDB();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000 || 10000;
 
 app.use(express.json());
 app.use(cors());
+const allowedOrigins = ["https://todo-kenma.netlify.app"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use("/api/tasks", taskRoutes);
 app.use("/api/users", userRoutes);
 app.use(errorHandler);
